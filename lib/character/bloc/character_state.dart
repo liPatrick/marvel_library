67d417bc5c1c31e2d1fdfe0358b6cb56a7bd52d0
Patrick Li <patrick8289@gmail.com>
2021-07-30 17:01:22 -0700

    changes to character state

--- lib/character/bloc/character_state.dart
+++ lib/character/bloc/character_state.dart
@@ -4,6 +4,11 @@ enum CharacterStatus { initial, success, failure }
 
 @immutable
 class CharacterState {
-  const CharacterState({this.characters = const <Character>[]});
+  const CharacterState({
+    this.status = CharacterStatus.initial,
+    this.characters = const <Character>[],
+  });
+
   final List<Character> characters;
+  final CharacterStatus status;
 }
